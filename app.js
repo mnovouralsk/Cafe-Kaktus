ca0ca5bd6f938b38b5e2a4ca837aa2f9d7c20523
mnovouralsk <34658786+mnovouralsk@users.noreply.github.com>
2024-03-25 20:27:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ fetch('https://mnovouralsk.github.io/Cafe-Kaktus/products.json')
             const productData = document.createElement('div');
             productData.className = 'product-details';
 
-            productData.innerHTML = '<h2 class="product-name">'+product.name+'</h2>'
+            productData.innerHTML = '<h5 class="product-name">'+product.name+'</h5>'
             productData.innerHTML += '<p class="product-composition">Состав: <span class="composition-text">'+product.composition+'</span></p>';
             productData.innerHTML += '<p class="product-price">'+product.price+' руб</p>';
 
